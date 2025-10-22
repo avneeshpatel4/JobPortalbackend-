2e12e37931e944378d2fcddd387ed38b69243a8d
Avneesh Patel <avneeshpatel20842@gmail.com>
2025-10-22 14:35:04 +0530

    For live api on render

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,13 +5,16 @@ import {
   register,
   updateProfile,
 } from "../controllers/user.controller.js";
-import authenticatedToken from "../middleware/isAuthenticated.js";
+import authenticateToken from "../middleware/isAuthenticated.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
-router.route("/profile/update").post(authenticatedToken, updateProfile);
+router
+  .route("/profile/update")
+  .post(authenticateToken, singleUpload, updateProfile);
 
-export default router
+export default router;
